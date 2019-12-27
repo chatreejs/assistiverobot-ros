c4edabb42625c95b2cdcf81ba8edbeb9526614a3
Chanon Treemeth <chanon29060@live.com>
2019-12-27 22:51:55 +0700

    Build Workspace

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/melodic/lib"
-export PKG_CONFIG_PATH="/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/chatree/ROS-Robotics/build"
-export ROS_PACKAGE_PATH="/home/chatree/ROS-Robotics/src:/opt/ros/melodic/share"
