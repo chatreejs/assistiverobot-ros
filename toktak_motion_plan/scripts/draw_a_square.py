dc24655efad30392f0065769d604e825536b8864
Chanon Treemeth <chanon29060@live.com>
2020-01-20 22:27:34 +0700

    Add motion plan to toktak

--- toktak_motion_plan/scripts/draw_a_square.py
+++ toktak_motion_plan/scripts/draw_a_square.py
@@ -14,7 +14,7 @@ class DrawASquare():
         # What to do you ctrl + c    
         rospy.on_shutdown(self.shutdown)
         
-        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        self.cmd_vel = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
      
 	# 5 HZ
         r = rospy.Rate(5);
